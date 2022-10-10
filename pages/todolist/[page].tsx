24693d5115271c5a6d8735cfe2699a8407685e98
ys <lhyahoo0119@gmail.com>
2022-10-10 20:00:21 +0900

    [Feat] dynamic routing

--- pages/todolist/[page].tsx
+++ pages/todolist/[page].tsx
@@ -1,9 +1,12 @@
 //api 호출 없이 전체 list 가져와서 getServerSideProps로 pagination 하는 page
-
+import { useState } from 'react';
 import axios from 'axios';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { useRouter } from 'next/router';
 import { TodoItem } from '../../interfaces/';
+import Todo from '../../components/Todo/Todo';
+import { Box, Pagination } from '@mui/material';
+import { theme } from '../../styles/theme';
 
 const Page = ({
   data,
@@ -12,23 +15,29 @@ const Page = ({
   const page = router.query.page;
   const perPage = 5;
   const checkDev = process.env.NEXT_PUBLIC_DEV;
-  let initialData;
-  if (checkDev) {
-    initialData = data.lists;
-  } else {
-    initialData = data;
-  }
+  const [todolist, setTodolist] = useState<TodoItem[]>(
+    checkDev ? data.lists : data
+  );
 
-  // props로 total 값 받기
-  // const totalTodos =
-  // const totalPages = totalTodos / perPage // 정수처리하기
-  // const start = (page -1) * perPage
-  // const end = start + perPage
-  // if(end > totalTodos){
+  const offset = (Number(page) - 1) * perPage;
 
-  // }
-
-  return <div>dynamic routing!</div>;
+  return (
+    <div>
+      <Box
+        sx={{
+          backgroundColor: theme.palette.primary.main,
+          width: 400,
+          boxShadow: 2,
+          margin: '1rem',
+          padding: 2,
+        }}
+      >
+        {todolist.slice(offset, offset + perPage).map((todo) => {
+          return <Todo key={todo._id} todo={todo} setTodolist={setTodolist} />;
+        })}
+      </Box>
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
